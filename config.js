2273302987720b372596b2093a905d9a809f85f5
Sanjeewar255 <sanjeewar255@gmail.com>
2025-08-28 22:43:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,9 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/DANUWA-MD/DANUWA-MD/blob/main/images/DANUWA-MD.png?raw=true",
-ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now😍*",
-BOT_OWNER: '94776121326',  // Replace with the owner's phone number
+ALIVE_IMG: process.env.ALIVE_IMG || "https://github.com/Sanjeewar255/Ruwan-X-MD-/blob/277deef234c557aff7b5e97591821b86c1ea66e9/Images/IMG-20250828-WA0004.jpg",
+ALIVE_MSG: process.env.ALIVE_MSG || "*Hello👋 DANUWA-MD Is Alive Now🥰*",
+BOT_OWNER: '94775646882',  // Replace with the owner's phone number
 
 
 
